c49d8418f074eaed1d3de6fbece35625e055623f
Juan Galarce <j6alarce@gmail.com>
2021-01-13 12:12:32 -0300

    Creacion de servicio REST para obtener ticket actuales

--- models/server.js
+++ models/server.js
@@ -19,11 +19,26 @@ class Server {
         
         // Configuraciones de sockets
         this.io = socketio( this.server, { /* configuraciones */ } );
+
+        // Inicializar socket
+        this.sockets = new Sockets( this.io )
     }
 
     middlewares() {
         // Desplegar el directorio público
         this.app.use( express.static( path.resolve( __dirname, '../public' ) ) );
+
+        // Configurar cors
+        this.app.use( cors() );
+
+        // GET de los ultimos tickets 
+        this.app.get('/last', ( req, res) => {
+
+            res.json({
+                ok: true,
+                last: this.sockets.ticketList.last13
+            })
+        })
 
         // CORS
         this.app.use( cors() );
@@ -32,17 +47,15 @@ class Server {
 
     // Esta configuración se puede tener aquí o como propieda de clase
     // depende mucho de lo que necesites
-    configurarSockets() {
-        new Sockets( this.io );
-    }
+    //configurarSockets() {
+    //new Sockets( this.io );
+    //}
 
     execute() {
 
         // Inicializar Middlewares
         this.middlewares();
 
-        // Inicializar sockets
-        this.configurarSockets();
 
         // Inicializar Server
         this.server.listen( this.port, () => {
